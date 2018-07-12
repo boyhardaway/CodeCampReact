0196529784bd8edfe5c181857c8673b45549e489
ronnapoom chanmol <boy.hardaway@gmail.com>
2018-07-12 22:01:46 +0700

    a

---
+++ react-day-24-3/src/component/Card.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Card extends Component{
+    render(){
+        retrun (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Card
